10f80d1eda0d21e805bd4f8e2eed06f9710d6112
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-06 18:21:16 +0200

    Docs for deki-tween from deki-editor 9500697

--- navtreedata.js
+++ navtreedata.js
@@ -26,8 +26,7 @@ var NAVTREE =
 [
   [ "deki-tween", "index.html", [
     [ "Components", "index.html#autotoc_md0", null ],
-    [ "Feature flags", "index.html#autotoc_md1", null ],
-    [ "Component reference", "index.html#autotoc_md2", [
+    [ "Component reference", "index.html#autotoc_md1", [
       [ "TweenComponent", "index.html#c-tweencomponent", null ]
     ] ]
   ] ]
--- navtreeindex0.js
+++ navtreeindex0.js
@@ -3,7 +3,6 @@ var NAVTREEINDEX0 =
 "index.html":[],
 "index.html#autotoc_md0":[0],
 "index.html#autotoc_md1":[1],
-"index.html#autotoc_md2":[2],
-"index.html#c-tweencomponent":[2,0],
+"index.html#c-tweencomponent":[1,0],
 "pages.html":[]
 };
